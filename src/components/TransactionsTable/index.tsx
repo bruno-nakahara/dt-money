637619ed1b960a84ff4daf40e7bc8ddc9c222705
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-05-01 20:39:49 -0300

    estrutura da aplicação / componentização / api get

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionTable() {
+    useEffect(() => {
+        api.get('transactions').then(response => console.log(response.data))
+    }, []);
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr></tr>
+                        <th>Título</th>
+                        <th>Preço</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Trabalho</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.200</td>
+                        <td>Casa</td>
+                        <td>29/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
